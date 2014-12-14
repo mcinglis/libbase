8dd811cb86ceb912bddd004c5ca9448b6efd5213
Malcolm Inglis <me@minglis.id.au>
2014-12-14 12:59:24 +1000

    Add license preamble to source files

--- bool.h
+++ bool.h
@@ -1,4 +1,22 @@
 
+// Copyright 2014  Malcolm Inglis <http://minglis.id.au>
+//
+// This file is part of Libbase.
+//
+// Libbase is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// Libbase is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Libbase. If not, see <https://gnu.org/licenses/>.
+
+
 #ifndef LIBBASE_BOOL_H
 #define LIBBASE_BOOL_H
 
