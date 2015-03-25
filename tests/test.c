738c0f724b7344bc4954eee9f8deb95c33bcbcc9
Malcolm Inglis <me@minglis.id.au>
2015-03-25 15:30:16 +1000

    Change order of arguments to 'clamp()'

--- tests/test.c
+++ tests/test.c
@@ -1,4 +1,5 @@
 
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,6 +15,7 @@ int
 main( void )
 {
     size_t const num_tests = 1000;
+
     for ( size_t i = 0; i < num_tests; i++ ) {
         int const x = rand();
         int const y = rand();
@@ -37,7 +39,8 @@ main( void )
                 int__min( x, y, z ) == int__min2( x, int__min2( y, z ) ),
                 int__min( x, y, z ) <= int__max( x, y, z ) );
     }
-    printf( "All `int` assertions passed.\n" );
+    printf( "`int` assertions passed.\n" );
+
     for ( size_t i = 0; i < num_tests; i++ ) {
         uchar const x = rand() % uchar__max_bound();
         uchar const y = rand() % uchar__max_bound();
@@ -64,7 +67,26 @@ main( void )
                 uchar__compare_i( 'x', 'X' ) == EQ,
                 uchar__compare_i( 'x', 'Y' ) == LT );
     }
-    printf( "All `uchar` assertions passed.\n" );
+    printf( "`uchar` assertions passed.\n" );
+
+    ASSERT( int__clamp( 5, 0, 10 ) == 5,
+            int__clamp( 0, 0, 10 ) == 0,
+            int__clamp( -1, 0, 10 ) == 0,
+            int__clamp( -1, -1, 10 ) == -1,
+            int__clamp( 9, 0, 10 ) == 9,
+            int__clamp( 10, 0, 10 ) == 10,
+            int__clamp( 11, 0, 10 ) == 10,
+            int__clamp( INT_MIN, 0, 10 ) == 0,
+            int__clamp( INT_MAX, 0, 10 ) == 10,
+            uchar__clamp( 5, 0, 10 ) == 5,
+            uchar__clamp( 0, 0, 10 ) == 0,
+            uchar__clamp( 1, 0, 10 ) == 1,
+            uchar__clamp( 9, 0, 10 ) == 9,
+            uchar__clamp( 10, 0, 10 ) == 10,
+            uchar__clamp( 11, 0, 10 ) == 10,
+            uchar__clamp( UCHAR_MAX, 0, 10 ) == 10 );
+    printf( "`clamp()` assertions passed.\n" );
+
 }
 
 
